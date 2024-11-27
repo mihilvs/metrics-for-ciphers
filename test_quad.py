1cbd39bd84713354bfdadb536bcdcafbbc12f892
mihilvs <157837292+mihilvs@users.noreply.github.com>
2024-11-26 19:11:10 -0500

    Add files via upload

---
+++ test_quad.py
@@ -0,0 +1,37 @@
+import random
+import os
+import sys
+
+
+def determine_bijection(a,b,c, M):
+    found = set()
+    for x in range(0, M):
+        found.add((a * (x**2) + b * x + c * (x**3)) % M)
+    return len(found) == M
+    
+def find_similar(a,b,c, M):
+    found = [] 
+    for i in range(0, M):
+        for j in range(0, M):
+            if determine_bijection(0, i, 0, M):
+                temp_found = True
+                for x in range(0, M):
+                    if (a * (x**2) + b * x + c * (x**3)) % M != (i * x + j) % M:
+                        temp_found = False 
+                if temp_found: found.append((0, i, j)) 
+    if found: 
+        print(f"{(c, b, a)} is the same function as {found}")
+    else:
+        print(f"{(c, b, a)} is not affine")
+def main():
+    M = 24
+    for c in range(0, 1):
+        for a in range(1, M):
+            for b in range(0, M):
+                if determine_bijection(a, b, c, M):
+                    find_similar(a, b, c, M)
+    print("DONE")
+
+
+if __name__ == "__main__":
+    main()
